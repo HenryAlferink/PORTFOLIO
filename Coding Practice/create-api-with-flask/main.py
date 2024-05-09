07a5b89348f13b2ca1c3d397c68e631ede2ab8bd
Henry Alferink <alferinkhenry@gmail.com>
2024-05-09 18:31:42 +1200

    api is now working - try it out with test.py

--- Coding Practice/create-api-with-flask/main.py
+++ Coding Practice/create-api-with-flask/main.py
@@ -1,16 +1,43 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
+# import json data into Python dictionary
+with open("cheeses.json", "r") as file:
+    cheeses = json.load(file)
+
 
 class Cheese(Resource):
-    def get(self, name):
-        return {}
+    def get(self, cheese):
+        return {"data": {cheese: cheeses[cheese]}}
+
+    def post(self):
+        data = request.json
+        cheese = data["cheese"]
+        cheeses[cheese] = {
+            "age_years": data["age_years"],
+            "country": data["country"],
+            "hardness": data["hardness"],
+        }
+        with open("cheeses.json", "w") as file:
+            file.write(json.dumps(cheeses, indent=4))
+        return {"message": f"Cheese {cheese} added successfully"}
+
+    def delete(self):
+        cheese = request.json["cheese"]
+        del cheeses[cheese]
+        with open("cheeses.json", "w") as file:
+            file.write(json.dumps(cheeses, indent=4))
+        return {"message": f"Cheese {cheese} deleted successfully"}
 
 
-api.add_resource(Cheese, "/cheese")
+# define which endpoints can use the given methods
+api.add_resource(Cheese, "/cheese/<string:cheese>", "/cheese")
+
+file.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Coding Practice/create-api-with-flask/test.py
+++ Coding Practice/create-api-with-flask/test.py
@@ -1,5 +1,17 @@
 import requests
 
-response = requests.get("http://")
+# add a new cheese to the data file
+response = requests.post(
+    "http://127.0.0.1:5000/cheese",
+    json={"cheese": "Gouda", "age_years": 0.5, "country": "Netherlands", "hardness": 5},
+    headers={"Content-Type": "application/json"},
+)
+print(response.json(), sep="\n")
 
-print(response.json())
+# return data for the "Cheddar" cheese
+response = requests.get("http://127.0.0.1:5000/cheese/Cheddar")
+print(response.json(), sep="\n")
+
+# delete the "Gouda" cheese from the data file
+response = requests.delete("http://127.0.0.1:5000/cheese", json={"cheese": "Gouda"})
+print(response.json(), sep="\n")
